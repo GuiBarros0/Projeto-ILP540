7c823f9cc5625202ff6fd7747f11c5ae4a4ed33a
Guilherme Barros <86804512+GuiBarros0@users.noreply.github.com>
2022-03-03 12:41:38 -0300

    Update index.php

--- index.php
+++ index.php
@@ -7,35 +7,57 @@
 <meta charset="utf-8" />
 <meta name=" viewport" content="width=device-width, initial-scale=1.0" />
 <title>Test</title>
+<link rel="stylesheet" href="css/bootstrap/bootstrap.min.css">
+<link href="signin.css" rel="stylesheet">
+<meta name="theme-color" content="#7952b3">
+
+
+    <style>
+      .bd-placeholder-img {
+        font-size: 1.125rem;
+        text-anchor: middle;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        user-select: none;
+      }
+
+      @media (min-width: 768px) {
+        .bd-placeholder-img-lg {
+          font-size: 3.5rem;
+        }
+      }
+    </style>
 </head>
-<body>
-<div>
-<ul>
-<li><a href="#Login" accesskey="1">[1] Login</a></li>
-<li><a href="#Menu" accesskey="2">[2] Menu</a></li>
-<li><a href="#Principal" accesskey="3">[3] Principal</a></li>
-<li><a href="#rodape" accesskey="4">[4] Rodape</a></li>
+<body class="text-center">
+<h1>To Do List</h1>
+<main class="form-signin">
+        <form>
+          <img class="mb-4" src="/docs/5.1/assets/brand/bootstrap-logo.svg" alt="" width="72" height="57">
+          <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
+      
+          <div class="form-floating">
+            <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+            <label for="floatingInput">Email address</label>
+          </div>
+          <div class="form-floating">
+            <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+            <label for="floatingPassword">Password</label>
+          </div>
+      
+          <div class="checkbox mb-3">
+            <label>
+              <input type="checkbox" value="remember-me"> Remember me
+            </label>
+          </div>
+          <button class="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
+          <p class="mt-5 mb-3 text-muted">&copy; 2017–2021</p>
+        </form>
+      </main>
 
-
-
-</ul>
-</div>
-<header>
-<div id="Login">
-<form action="#" method="post">
-<label>Usuario:<input type="text" name="usuario" id="usuario" /></label>
-<label>Senha:<input type="password" name="Senha" id="senha" /></label>
-</form>
-</div>
-<nav id="menu">
-<ul>
-<li><a href="intex.html">Home</a></li>
-<li><a href="Contato.html">Contato</a></li>
-</ul>
-
-
-
-</nav>
-</header>
+          <script src="js/jquery-3.3.1.slim.min.js"></script>
+	
+          <script src="js/popper.min.js" ></script>
+          
+          <script src="js/bootstrap.min.js"></script>
 </body>
 </html>
